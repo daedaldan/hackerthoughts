0b39636a75977ff3cc7f172a10c14dd4959c7efc
Daniel Wang <daniel22373@gmail.com>
2020-12-14 18:51:16 -0500

    Set up CSS, get ready to fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,57 @@
-var path = require("path");
+var path = require('path');
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: path.join(__dirname, './src/index.js'),
+module.exports = function(_env, argv) {
+  const isProduction = argv.mode === 'production';
+  const isDevelopment = !isProduction;
 
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: '[name]-[hash].js'
-  },
+  return {
+    entry: path.join(__dirname, './src/index.js'),
 
-  plugins: [
-      new BundleTracker({
-        path: __dirname,
-        filename: 'webpack-stats.json'
-      }),
-  ],
+    output: {
+      path: path.join(__dirname, './dist'),
+      filename: '[name]-[hash].js',
+      publicPath: '/'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            cacheDirectory: true,
-            cacheCompression: false,
+    plugins: [
+        new BundleTracker({
+          path: __dirname,
+          filename: 'webpack-stats.json'
+        }),
+        new MiniCssExtractPlugin({
+          filename: './src/css/[name].[contenthash:8].css',
+          chunkFilename: './src/css/[name].[contenthash:8].chunk.css'
+        })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+              envName: isProduction ? 'production' : 'development'
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: [
+              isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'
+          ]
         }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  };
 };
+
