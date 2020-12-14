cdbde97828517dacdc3a7e19dd20dd9f23244c5c
Daniel Wang <daniel22373@gmail.com>
2020-12-14 17:22:23 -0500

    Set up Webpack (for real)

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins: [
       new BundleTracker({
         path: __dirname,
-        filename: 'webpack-stats,json'
+        filename: 'webpack-stats.json'
       }),
   ],
 };
